fe025ab37ffde7457cc4914de876f4e9b084ecac
Jacobo de Vera <devel@jacobodevera.com>
2013-02-08 13:07:37 +0000

    Export line type names in the main module

--- unidiff/__init__.py
+++ unidiff/__init__.py
@@ -3,5 +3,7 @@
 
 from parser import UnidiffParseException, parse_unidiff
 from patch import PatchSet, PatchedFile, Hunk
+from patch import LINE_TYPE_ADD, LINE_TYPE_DELETE, LINE_TYPE_CONTEXT
+
 
 VERSION = (0, 1, 0)
