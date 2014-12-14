722289f823147a9c6a1aab9e391bb82a0243d595
matiasb <mbordese@gmail.com>
2014-12-14 20:52:26 -0200

    Minor setup.py updates.

--- setup.py
+++ setup.py
@@ -10,12 +10,22 @@ from unidiff import VERSION
 setup(
     name='unidiff',
     version=VERSION,
-    description="Unified diff parsing library.",
-    keywords='unified diff parse',
+    description="Unified diff parsing/metadata extraction library.",
+    keywords='unified diff parse metadata',
     author='Matias Bordese',
     author_email='mbordese@gmail.com',
     url='http://github.com/matiasb/python-unidiff',
     license='MIT',
     packages=find_packages(),
     scripts=['bin/unidiff'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 4 - Beta'
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
 )
